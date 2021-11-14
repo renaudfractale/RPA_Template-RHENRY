c278e8bcbf56894f40061cde7ccaf7c76e59cd2e
Renaud HENRY <git@rhenry.fr>
2021-11-14 17:27:55 +0100

    Update Site Web VueJS3

---
+++ Documentation/Configuration du Robot.mm
@@ -0,0 +1,99 @@
+<map version="freeplane 1.9.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Configuration&#xa;du Robot" FOLDED="false" ID="ID_696401721" CREATED="1610381621824" MODIFIED="1636875226232" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle" zoom="2.143">
+    <properties fit_to_viewport="false" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_271890427" ICON_SIZE="12 pt" COLOR="#000000" STYLE="fork">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_271890427" STARTARROW="DEFAULT" ENDARROW="NONE"/>
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+<richcontent CONTENT-TYPE="plain/auto" TYPE="DETAILS"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" BACKGROUND_COLOR="#4e85f8" STYLE="bubble" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#4e85f8"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_67550811">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#003399" TRANSPARENCY="255" DESTINATION="ID_67550811"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="2" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Core" POSITION="right" ID="ID_1738022909" CREATED="1636875231759" MODIFIED="1636875235457">
+<edge COLOR="#ff0000"/>
+<node TEXT="Projet" ID="ID_1557655310" CREATED="1636875294174" MODIFIED="1636875675552">
+<cloud COLOR="#f0f0f0" SHAPE="ARC"/>
+<node TEXT="Code Projet" ID="ID_20490271" CREATED="1636875337551" MODIFIED="1636875344488"/>
+<node TEXT="Nom du programme" ID="ID_737873204" CREATED="1636875345679" MODIFIED="1636875352617"/>
+<node TEXT="Unité" ID="ID_1475954489" CREATED="1636875353175" MODIFIED="1636875364744"/>
+<node TEXT="Environnent (par Asset ou argument)" ID="ID_1235218898" CREATED="1636875365128" MODIFIED="1636875671510"/>
+<node TEXT="Pays" ID="ID_1931513686" CREATED="1636875516470" MODIFIED="1636875520072"/>
+<node TEXT="Région du monde" ID="ID_765337644" CREATED="1636875520575" MODIFIED="1636875526831"/>
+</node>
+<node TEXT="Paramètres Core" ID="ID_1085191256" CREATED="1636875300535" MODIFIED="1636875679633">
+<cloud COLOR="#f0f0f0" SHAPE="ARC"/>
+<node TEXT="Config Log  NoSQL" ID="ID_1341307245" CREATED="1636875410519" MODIFIED="1636875427696"/>
+<node TEXT="Config Log Texte" ID="ID_1585532285" CREATED="1636875428351" MODIFIED="1636875440481"/>
+<node TEXT="Config Env" ID="ID_976675006" CREATED="1636875548146" MODIFIED="1636875556241"/>
+<node TEXT="Config QueueListe" ID="ID_594891409" CREATED="1636875556567" MODIFIED="1636875583248"/>
+</node>
+</node>
+<node TEXT="Paramètres" POSITION="left" ID="ID_521490039" CREATED="1636875236688" MODIFIED="1636875693044">
+<edge COLOR="#0000ff"/>
+<cloud COLOR="#f0f0f0" SHAPE="ARC"/>
+<node TEXT="Paramètres de l&apos;Env" ID="ID_1184318788" CREATED="1636875591159" MODIFIED="1636875600512"/>
+<node TEXT="Constantes" ID="ID_1990840233" CREATED="1636875601911" MODIFIED="1636875613537"/>
+<node TEXT="Assets" ID="ID_1989630084" CREATED="1636875614022" MODIFIED="1636875624303"/>
+</node>
+</node>
+</map>
--- SiteWeb/cli-vuejs3/website/src/views/Infos.vue
+++ SiteWeb/cli-vuejs3/website/src/views/Infos.vue
@@ -14,11 +14,11 @@
 
             <div class="col s12 m6">
               <p>
-                Le ROBOT ENTREPRISE FRAMEWORK VANILLA (REF ou REFV) proposé par UIPATH est un
-                outil de base pour implémenter des processus.
+                Le ROBOT ENTREPRISE FRAMEWORK VANILLA (REF ou REFV) proposé par
+                UIPATH est un outil de base pour implémenter des processus.
                 <br />
                 Les tâches des processus robotisés sont atomiser en
-                TransactionItem dans un file d’attente. Cela permet d’avoir une
+                TransactionItem dans une file d’attente. Cela permet d’avoir une
                 granulosité fine dans l’analyse du comportement des robots.
                 <br />
                 Le REF se décompose de 4 états : Etat 1 : Initialization <br />
@@ -102,14 +102,109 @@
         • Cette état permet clore le programme ,d’exécuter l’envoi de l’email
         récapitulatif et de lancer une procédures à la fin du robot<br />
       </p>
+      <h6 class="header orange-text">
+        2.3) Structure des dossiers Modernes dans l’Orchestrateur
+      </h6>
+      <img src="../assets/info/Image1.png" height="500" class="center" />
 
       <div class="container">
         <div class="section">
           <div class="row">
-            <div class="col s12 m6"></div>
+            <div class="col s12 m6">
+              <h6 class="center">
+                Le chargement de la configuration se fait en cinq phases :
+              </h6>
+              <img src="../assets/info/fusee.png" height="350" class="center" />
+            </div>
+            <div class="col s12 m6">
+              <br /><br />
+              <p>1) Phase 1 : Chargement des données primitives</p>
+              <br />
+              <p>2) Phase 2 : Construction des paternes</p>
+              <br />
+              <p>
+                3) Phase 3 : Chargement de la configuration des systèmes annexes
+              </p>
+              <br />
+              <p>
+                4) Phase 4 : Chargement de la configuration lié aux constantes
+                et l’environnement
+              </p>
+              <p>5) Phase 5 : Chargement des ressources (Assets)</p>
+            </div>
           </div>
         </div>
       </div>
+
+      <h7 class="header orange-text"
+        >2.3.1) Phase 1 : Chargement des données primitive</h7
+      >
+      <p>
+        Ici on charge les paramètres projet core, c’est-à-dire : <br />
+        - Le code Projet => exemple : « RPA0001 » <br />
+        - Le nom du programme => exemple : « Demo REFC » <br />
+        - L’unité, c’est le nom du pôle qui utilise le robot => exemple «
+        FINANCE » ou « RH » pour Ressources Humaine <br />
+        - L’environnement du robot, soit DEV, UAT, PROD ou autre. Cette variable
+        est mise à jour via une ressource avec une valeur personnalisé, mais
+        c’est l’argument Env du programme MAIN qui à le dernier mot. Donc on
+        peut forcer l’environnement d’un robot via l’api de lancement.
+        Intéressant si on veut comparer le fonctionnement de deux environnements
+        sur une même machine sans toucher le paramètre générale qui est la
+        ressource avec une valeur personnalisé <br />
+        - Le pays où fonctionne le robot , exemple « France » <br />
+        - La région du monde du robot, exemple « Europe » <br />
+        <i
+          >Il faut noter que le pays et la région du monde n’est pas utilisé par
+          la suite, mais si le paterne de la structure des dossiers se
+          complexifie, le fait que les données pays et région du monde soit déjà
+          renseigné simplifiera la migration d’une structure simple à
+          complète</i
+        >
+      </p>
+      <h7 class="header orange-text"
+        >2.3.2) Phase 2 : Construction des paternes
+      </h7>
+      <p>
+        En utilisant les ressources du dossier CONFIG, le robot construit le
+        chemin d’accès à la file d’attende, le nom de la file d’attende et les
+        mails d’envoie des informations d’erreurs et de récapitulatif en
+        fonction des informations initialisées par l’étape 1
+      </p>
+      <h7 class="header orange-text"
+        >2.3.3) Phase 3 : Chargement de la configuration des systèmes annexes
+      </h7>
+      <p>
+        En utilisant les ressources du dossier CONFIG, le robot charge les
+        paramètres de connexion à la base NOSQL, du dossier des logs Externe et
+        autres systèmes d’informations
+      </p>
+      <h7 class="header orange-text"
+        >2.3.4) Phase 4 : Chargement de la configuration lié aux constantes et à
+        l’environnement
+      </h7>
+      <p>
+        En utilisant les ressources du fichier de configuration , on charge en
+        premier temps les constantes et en dernier les paramètre de
+        l’environnement. Donc les paramètres d’environnement peut écraser les
+        valeur des constantes.
+      </p>
+      <h7 class="header orange-text"
+        >2.3.5) Phase 5 : Chargement des ressources (Assets)
+      </h7>
+      <p>
+        En utilisant les ressources listées dans le fichier de configuration
+        excel, on charge avec un chemin d’accès soit paramétré, soit en dur,
+        soit dynamique. <br />
+        - Quand le chemin est paramétré, les noms entre crochet sont remplacés
+        par les valeurs des données Core (voir étape 1)<br />
+        - Quand le chemin est en dure, on prend l’expression littéral<br />
+        - Quand le chemin est en dynamique, le robot décompose le path du robot
+        et à chaque dossier de l’Orchestrator, le système essaye de récupérer la
+        ressource. Donc c’est le dernier dossier, le dossier de la file
+        d’attente qui réécrit en dernier les données de la ressource.<br />
+      </p>
+      <img src="../assets/info/Image2.png" height="100" class="center" />
     </div>
   </div>
 </template>
